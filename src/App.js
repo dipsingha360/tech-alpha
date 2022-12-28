52f6d5207161eed04254d9fb24fccdaef910cb69
Dip Singha <dipsingha2@gmail.com>
2022-12-29 02:45:33 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Products from "./pages/Products";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="app min-h-screen bg-gray-50 text-gray-700 ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-bg  bg-teal-500 text-teal-50 h-20 flex justify-center items-center ">
+      <div className="navbar container mx-auto flex items-center justify-between">
+        <div className="left">
+          <span className="logo text-xl font-semibold">
+            tech<span className="text-gray-700">Alpha</span>{" "}
+          </span>
+        </div>
+        <div className="right flex items-center gap-5 ">
+          <Link
+            end
+            to="/"
+            className="nav-link hover:text-teal-300 duration-300"
+          >
+            Home
+          </Link>
+          <Link
+            to="/products"
+            className="nav-link hover:text-teal-300 duration-300"
+          >
+            Products
+          </Link>
+          <Link to="/cart">
+            <span className="cart-icon relative">
+              <BsCart3 />
+              <span className="cart-counter text-teal-50 absolute -top-3 -right-3 text-xs bg-gray-700 h-5 w-5 rounded-full flex items-center justify-center ">
+                0
+              </span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
