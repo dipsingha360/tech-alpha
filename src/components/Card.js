a8540875a79d092da8e8bb1e2287ceccd1958b9e
Dip Singha <dipsingha2@gmail.com>
2023-01-02 11:30:50 +0600

    currency formatter added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+import { currencyFormatter } from "../utilities/currencyFormatter";
+
 const Card = ({ product }) => {
   return (
     <div className="product flex flex-col gap-3 shadow-md bg-white rounded-xl overflow-hidden hover:shadow-xl duration-300">
@@ -14,7 +16,7 @@ const Card = ({ product }) => {
         <p className="details text-gray-500 h-[6rem]">{product.description}</p>
         <div className="flex justify-between items-center">
           <span className="price text-xl font-medium text-teal-500">
-            ${product.price}
+            {currencyFormatter(product.price)}
           </span>
           <button className="uppercase bg-teal-500 text-teal-50 py-3 px-8 rounded-md shadow-md shadow-teal-50 hover:bg-teal-600 hover:shadow-lg duration-300 ">
             Add to cart
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Card from "../components/Card";
 
 const data = [
